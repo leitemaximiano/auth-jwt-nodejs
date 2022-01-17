4a4a27b6b91f86df428ce3d67c2cc444838b2123
Maximiano Leite <macio.pb.cg@gmail.com>
2022-01-17 04:28:05 -0300

    add secret

--- src/globalVaribles.js
+++ src/globalVaribles.js
@@ -5,5 +5,6 @@ dotenv.config();
 module.exports = {
     port: process.env.PORT,
     userDB: process.env.DB_USER,
-    passwordDB: process.env.DB_PASSWORD
+    passwordDB: process.env.DB_PASSWORD,
+    secret: process.env.SECRET
 };
